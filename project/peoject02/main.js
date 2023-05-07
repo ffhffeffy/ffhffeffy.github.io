c827a47fab1a1fb16ac7308d52506746dde573fa
ffhffeffy <hf2460@columbia.edu>
2023-05-07 13:39:18 -0400

    p2 js_updata

---
+++ project/peoject02/main.js
@@ -0,0 +1,83 @@
+console.log('this works')
+
+const flowers = [
+    {
+        artist: "Gareth Mason",
+        shape: "bottle",
+        color: "cold",
+        info: "O, 2016-2017\n13.8 x 7.1 x 6.7 in\nPorcelain, layered matte and celadon glazes",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_cold_bottle01.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bottle",
+        color: "cold",
+        info: "Sharp Intake, 2008-2011\n16.9 x 11.0 x 8.7 in\nPorcelain, stoneware, layered glazes",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_cold_bottle02.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bottle",
+        color: "cold",
+        info: "Buckle, 2017\n6.1 x 4.9 x 4.5 in\nPorcelain, fluid celadon glaze",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_cold_bottle03.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bowl&pot",
+        color: "cold",
+        info: "Private Sensation, 2009-2013\n18.5 x 24.8 x 23.2 in\nPorcelain, copper, dolomite matte glaze",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_cold_bowl01.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bottle",
+        color: "mix",
+        info: "A Gentle Collapsing, 2017-2018\n27.9 x 17.7 x 16.9 in\nStoneware, porcelain, glazes, 'wild' clay",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_mix_bottle01.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bottle",
+        color: "mix",
+        info: "Spike, 2019\n25.0 x 19.0 x 20.0 in\nPorcelain, stoneware, glazes",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_mix_bottle02.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bottle",
+        color: "warm",
+        info: "Chance Encounter, 2016-2018\n13.4 x 9.8 x 9.5 in\nStoneware, porcelain, glazes",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_warm_bottle01.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bottle",
+        color: "warm",
+        info: "Ribbed Red, 2017\n8.1 x 5.1 x 2.0 in\nPorcelain, stoneware, copper red, layered glazes",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_warm_bottle02.png"
+    },
+    {
+        artist: "Gareth Mason",
+        shape: "bowl&pot",
+        color: "warm",
+        info: "Warp, 2017\n5.9 x 13.6 x 11.0 in\nPorcelain, copper red glaze",
+        image: "images/ceramics/Gareth Mason/Gareth Mason_warm_bowl01.png"
+    },
+    {
+        artist: "Yunfei Zhang",
+        shape: "",
+        color: "yellow",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/800px-Sunflower_sky_backdrop.jpg"
+    },
+    {
+        artist: "Zheyuan Feng",
+        shape: "",
+        color: "blue",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/a/a8/Hyacinthoides_non-scripta_%28Common_Bluebell%29.jpg"
+    },
+
+];
+
